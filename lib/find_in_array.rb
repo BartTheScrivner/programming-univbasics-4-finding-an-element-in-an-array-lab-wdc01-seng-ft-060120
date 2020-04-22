1b2b519d982286f91d70bdaca0575c54418be9f4
fantastic-scope-1514 <dframe22@gmail.com>
2020-04-22 01:16:29 +0000

    Automatically backed up by Learn

--- lib/find_in_array.rb
+++ lib/find_in_array.rb
@@ -1,4 +1,8 @@
 def find_element_index(array, value_to_find)
   # Add your solution here
-  array[value_to_find]
+  count = 0
+  while count < array.length do 
+    if array[count] == value_to_find
+      count
+    end
 end
